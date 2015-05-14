2562d206370128fe9493f5a48dc52a8e17085ff2
Derek Lieu <dl@dereklieu.com>
2015-05-14 19:02:09 -0400

    Uses should; tests three routes

--- test/spec/routes.js
+++ test/spec/routes.js
@@ -1,13 +1,27 @@
 'use strict';
 
 describe('Wrapper routes', function() {
+  var router = app_router;
+  var $main = $('#main-frame');
 
   describe('Editor route', function() {
     it('Should call the editor with the appropriate route data', function() {
-      AppRouter.prototype.editor = sinon.spy();
-      var router = new AppRouter();
-      router.navigate('editor');
-      assert(router.editor.called);
+      router.navigate('editor/123', {trigger: true});
+      $main.attr('src').should.eql('//devseed.com/openroads-iD/123');
+    });
+  });
+
+  describe('Analytics route', function() {
+    it('Should call the dashboard with the appropriate route data', function() {
+      router.navigate('analytics/abc', {trigger: true});
+      $main.attr('src').should.eql('//devseed.com/openroads-analytics/abc');
+    });
+  });
+
+  describe('Verification route', function() {
+    it('Should call to-fix with the appropriate route data', function() {
+      router.navigate('verification/a/b#c', {trigger: true});
+      $main.attr('src').should.eql('//devseed.com/to-fix/a/b#c');
     });
   });
 
